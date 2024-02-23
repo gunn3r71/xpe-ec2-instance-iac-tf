06db1e009292179fcb2473ef7f14ba6f76760c02
Lucas pereira <lucasgunner3@gmail.com>
2024-02-23 01:04:59 -0300

    fix: terraform required version scope

--- terraform-without-modules/main.tf
+++ terraform-without-modules/main.tf
@@ -1,12 +1,12 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 4.16"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
-
-    required_version = ">= 1.2.0"
   }
+
+  required_version = ">= 1.2.0"
 }
 
 provider "aws" {
