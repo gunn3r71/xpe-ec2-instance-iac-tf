1b97391722d3e033e52776c6a900e0b76fc5e821
Lucas pereira <lucasgunner3@gmail.com>
2024-02-23 01:04:59 -0300

    fix: updating version

--- terraform-without-modules/main.tf
+++ terraform-without-modules/main.tf
@@ -1,11 +1,11 @@
 terraform {
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 4.16"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
 
-    required_version = ">= 1.2.0"
+    required_version = ">= 1.7.4"
   }
 }
 
