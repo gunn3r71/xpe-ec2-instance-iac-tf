1170ae58b16500027cf3342a855747d6da3e6dc0
Lucas pereira <lucasgunner3@gmail.com>
2024-02-23 23:24:24 -0300

    feat: changing instance name

--- terraform-with-modules/main.tf
+++ terraform-with-modules/main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "xpe-ec2-tf-instance" {
   ami = var.imageId
   instance_type = var.instance_type
   key_name = var.key_name
-  
+
   vpc_security_group_ids = ["${module.xpe-ec2-sg.sg-id}"]
 
   tags = {
@@ -29,7 +29,7 @@ resource "aws_instance" "xpe-ec2-tf-instance" {
 resource "aws_eip" "xpe-ec2-tf-eip" {
   instance = aws_instance.xpe-ec2-tf-instance.id
   tags = {
-    Name = "ec2-eip-terraform"
+    Name = "ec2-eip-terraform-with-outputs"
     Course = "XPe AWS"
   }
 }
