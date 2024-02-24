6353c6b4bdd49e901823b192de6070f35a7682ac
Lucas pereira <lucasgunner3@gmail.com>
2024-02-23 21:56:48 -0300

    feat: key name default

--- terraform-without-modules/variables.tf
+++ terraform-without-modules/variables.tf
@@ -9,12 +9,14 @@ variable "imageId" {
     type = string
     nullable = false
     description = "Imagem utilizada para criar instância EC2"
-    default = "ami-033606fe1cfd7fc48"
+    default = "ami-0ff8a91507f77f867"
 }
 
 variable "key_name" {
     type = string
     nullable = false
+    description = "Chave de acesso SSH"
+    default = "xpe-ec2-server"
 }
 
 variable "instance_type" {
