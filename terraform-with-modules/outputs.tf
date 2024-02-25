6f6b92f9095aea0ccf2719906963f77ed0d879da
Lucas pereira <lucasgunner3@gmail.com>
2024-02-25 20:28:32 -0300

    fix: disabling outputs

--- terraform-with-modules/outputs.tf
+++ terraform-with-modules/outputs.tf
@@ -1,7 +1,7 @@
-output "instance-id" {
-  value = "${xpe-ec2-instance.instance_id}"
-}
+# output "instance-id" {
+#   value = "${xpe-ec2-instance.instance_id}"
+# }
 
-output "tags" {
-  value = "${xpe-ec2-instance.tags}"
-}
+# output "tags" {
+#   value = "${xpe-ec2-instance.tags}"
+# }
